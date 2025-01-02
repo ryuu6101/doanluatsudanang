8828ec7de77ac144535aa97fbb644246a07f5b47
unknown <duylong0601@gmail.com>
2025-01-02 16:49:39 +0700

    update_0201

--- resources/views/admin/sections/file-manager/index.blade.php
+++ resources/views/admin/sections/file-manager/index.blade.php
@@ -7,7 +7,7 @@
 <div class="row">
     <div class="col">
         <iframe src="{{ url('responsive_filemanager/filemanager/dialog.php') }}?type=2&lang=vi" frameborder="0" 
-        class="w-100 border shadow" height="550"></iframe>
+        class="w-100 border shadow" height="530"></iframe>
     </div>
 </div>
 
